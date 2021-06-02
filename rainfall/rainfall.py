f8ea3f14b37592bdeb84d4beb11cee78b948e6f5
hinevics <hinevics@gmail.com>
2021-06-02 16:54:37 +0300

    add new file for work

--- rainfall/rainfall.py
+++ rainfall/rainfall.py
@@ -1,4 +1,18 @@
-import pytest
+"""
+data and data1 - две строки с записями осадков в нескольких городах за месяцы с января по декабрь. Записи городов разделяются \ n. После названия каждого города ставится ":".
+
+"""
+
+def mean(town, strng):
+    pass
 
 
-print('Hello word')
+def variance(town, strng):
+    pass
+
+def main():
+    print('Hello word')
+
+
+if __name__ == '__main__':
+    main()
